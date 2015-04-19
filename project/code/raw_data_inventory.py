2c8adbc045fe66b36a08a12673fc9dc55ec3d5c6
JEREMY WARD <jeremyleeward@gmail.com>
2015-04-19 12:04:23 -0400

    updated data inventory

--- project/code/raw_data_inventory.py
+++ project/code/raw_data_inventory.py
@@ -20,23 +20,38 @@ for (dirpath, dirname, files) in os.walk(cwd):
         thefile = os.path.join(dirpath, filename)
         list_of_files.append(thefile)
 
+# you might want to run this following line to make it explicit that the files are .txt files
+# ~~> after running, need to run the above again to update the inventory with the new names
+#for name in list_of_files:    
+#    os.rename(name, name + '.txt')
+    
+
 # remove non-project related portion of the path (in other words, change to relative path instead of absolute)
 # this will also allow the files to be accessed from the 'code' subdirectory of the project directory
 for ii in xrange(len(list_of_files)):
     jj = list_of_files[ii].find('/raw_data')
     list_of_files[ii] = '..' + list_of_files[ii][jj:]
  
-# print output to csv file
+# print inventory to csv file
 with open('raw_email_inventory.csv', 'wb') as f:
     wr = csv.writer(f)
     for item in list_of_files:
         wr.writerow([item]) # put item in list so that it will be written as a whole and not parsed
  
+# create sample set to test processing code
+with open('scratch_test_sample.csv', 'wb') as f:
+    wr = csv.writer(f)
+    for ii in xrange(len(list_of_files)):
+        if ii % 100 == 0:
+            wr.writerow([list_of_files[ii]])
+         
  
-# make sure csv was written correctly
+ 
+ 
+'''# make sure csv was written correctly
 with open('raw_email_inventory.csv', 'rb') as f:
     new_list = f.readlines()
 
 for j in new_list:
     print j
-
+'''
